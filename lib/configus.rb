5e1844bb5dafbfc98d9d5e653b8c61733311b98e
cafekoma <cafekoma@gmail.com>
2013-08-02 09:08:09 +0000

    added config

--- lib/configus.rb
+++ lib/configus.rb
@@ -4,5 +4,9 @@ module Configus
   
   autoload :Builder, 'configus/builder'
   autoload :HashConstructor, 'configus/hash_constructor'
+  autoload :Config, 'configus/config'
 
+  def self.build(default_env, &block)
+    @config = Builder.build(default_env, &block)
+  end
 end
--- lib/configus/builder.rb
+++ lib/configus/builder.rb
@@ -9,9 +9,16 @@ module Configus
       instance_eval &block
     end
 
+    def self.build(def_env, &block)
+      builder = new(def_env, &block)
+      Config.new(builder.result)
+    end
+
     def env(enviroment, &block)
       if block_given?
         self.conf_structure = HashConstructor.block_to_hash(block)
+      else
+        raise ArgumentError
       end
     end
 
